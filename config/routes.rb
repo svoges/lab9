4b472db3c455191754fd0e977888887ab0d7d6f4
rpflynn22 <rpflynn22@gmail.com>
2014-11-06 20:13:14 -0800

    change route for new quit

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   resources :users, only: [:show, :index]
   resources :quits
+  get '/users/:user_id/quits/new', to: 'quits#new'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
